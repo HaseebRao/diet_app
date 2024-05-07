f6154f7e9fcf92f311f3b883ae9ddca3712e739a
Cyber <maryamanjum340@gmail.com>
2024-05-07 16:15:20 +0500

    ghjg

---
+++ lib/module/authentication_module/question/binding/question_binding.dart
@@ -0,0 +1,18 @@
+
+import 'package:get/get.dart';
+
+import '../controller/question_controller.dart';
+
+class QuestionBinding implements Bindings{
+  @override
+  void dependencies() {
+    Get.lazyPut(() => QuestionController());
+  }
+
+}
+
+
+
+
+
+
