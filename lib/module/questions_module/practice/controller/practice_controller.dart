64b7bf9b349f04de838228f70d7be9cbc6fc4430
Cyber <maryamanjum340@gmail.com>
2024-05-07 16:14:27 +0500

    ghjg

---
+++ lib/module/questions_module/practice/controller/practice_controller.dart
@@ -0,0 +1,25 @@
+// PracticeController.dart
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class PracticeController extends GetxController {
+  // Rx variables to hold state
+  var selectedHour = RxInt(TimeOfDay.now().hour % 12);
+  var selectedMinute = RxInt(TimeOfDay.now().minute);
+  var selectedWeightUnitIndex = RxInt(
+      TimeOfDay.now().period == DayPeriod.am ? 0 : 1);
+
+  // Callback method for hour change
+  void handleHourChange(int hour) {
+    selectedHour.value = hour;
+  }
+
+  // Callback method for minute change
+  void handleMinuteChange(int minute) {
+    selectedMinute.value = minute;
+  }
+
+  void handleWeightUnitChange(int index) {
+    selectedWeightUnitIndex.value = index;
+  }
+}
