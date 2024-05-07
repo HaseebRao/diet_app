16fd7586bb70786ea54904a04ac19d509e6ccb72
Cyber <maryamanjum340@gmail.com>
2024-05-07 16:19:51 +0500

    ghjg

---
+++ lib/module/questions_module/q2/binding/q2_binding.dart
@@ -0,0 +1,52 @@
+
+
+
+
+
+//
+// Scaffold(
+// body: Padding(
+// padding: EdgeInsets.symmetric(horizontal: 21.h),
+// child: Column(
+// crossAxisAlignment: CrossAxisAlignment.start,
+// children: [
+// SizedBox(height: 47.h,),
+// SvgPicture.asset(Appasset.Arrowdiet,height: 17.h,),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Text(
+// Apptexts.textno,
+// style: TextStyle(
+// fontSize: 16.sp,
+// fontFamily: ApptextStyle.fontfamily,
+// color:AppColors.blackTextColor,
+// fontWeight: FontWeight.w700,
+// ),),
+// ],
+// ),
+// SizedBox(height: 8.h,),
+// LinearProgressIndicator(
+// value: 0.1, // 20% filled
+// minHeight: 11.h,
+// backgroundColor: Colors.grey[300],
+// borderRadius: BorderRadius.circular(10),
+// valueColor: AlwaysStoppedAnimation<Color>(AppColors.Profilecircle),
+// ),
+// SizedBox(height: 75.h,),
+// Center(
+// child: Text(
+// textAlign: TextAlign.center,
+// Apptexts.weight,
+// style: TextStyle(
+// fontSize: 21.sp,
+// fontFamily: ApptextStyle.fontfamily,
+// color:AppColors.blackTextColor,
+// fontWeight: FontWeight.w700,
+// ),),
+// ),
+//
+// ],
+// ),
+// ),
+// );
