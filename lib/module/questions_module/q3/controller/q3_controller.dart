1cd3122c64f734246517f0ae273397b2e7688afa
Cyber <maryamanjum340@gmail.com>
2024-05-08 12:52:00 +0500

    ghjg

---
+++ lib/module/questions_module/q3/controller/q3_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+class Q3Controller extends GetxController{
+
+  final RxInt selectedRadio = 1.obs;
+  RxBool isDarkMode = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  void setSelectedRadio(int val) {
+    selectedRadio.value = val;
+  }
+
+  void changeTheme() {
+
+    update();
+  }
+}
