dc13296df9548ceb3b9eac258c51bf8692decf01
Cyber <maryamanjum340@gmail.com>
2024-05-14 13:45:49 +0500

    ghjgghf

---
+++ lib/module/payment_module/subscriptionmethod/binding/subsmethod_binding.dart
@@ -0,0 +1,10 @@
+import 'package:diet_app/module/payment_module/subscriptionmethod/controller/subsmethod_controller.dart';
+import 'package:get/get.dart';
+
+class SubsmethodBinding implements Bindings{
+  @override
+  void dependencies() {
+   Get.lazyPut(() => SubsmethodController());
+  }
+
+}
---
+++ lib/module/payment_module/subscriptionmethod/view/subsmethod_view.dart
@@ -0,0 +1,21 @@
+import 'package:diet_app/module/payment_module/subscriptionmethod/controller/subsmethod_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+class SubsmethodView extends GetView<SubsmethodController>{
+  @override
+  Widget build(BuildContext context) {
+ return
+  Scaffold(
+    body: Column(
+
+      children: [
+
+
+      ],
+    ),
+  );
+  }
+
+}
